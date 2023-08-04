acb1d1d22f8d4355f566cde076dc6aa2db5278e0
shree <shree@DESKTOP-GEGVI44>
2023-08-04 15:20:59 +0530

    created  AddCustomer page by Sulbha Date

--- AllFeatureFiles/Telicom.feature
+++ AllFeatureFiles/Telicom.feature
@@ -1,4 +1,3 @@
-
 Feature: Telecom Application test cases.
 
   #home page created by Pranita
@@ -6,3 +5,10 @@ Feature: Telecom Application test cases.
     Given user on home page
     When user validatae url and title
     Then user click on Add Customer link
+
+# AddCustomerPage Created by Sulbha Date
+  Scenario: Test the AddCustomerDetails
+    When User Select Background Radio button
+    And User enter Firstname and Lastname and Email and Address and Mobilenumber
+    And User click on Submit button
+    Then User capture the customer ID
--- src/main/java/UtilsLayer/UtilsClass.java
+++ src/main/java/UtilsLayer/UtilsClass.java
@@ -1,44 +1,44 @@
 package UtilsLayer;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 
 import BaseLayer.BaseClass;
 
 public class UtilsClass extends BaseClass {
 
-		public static void sendKeys(WebElement wb,String value)
-		{
-			if(wb.isDisplayed()&& wb.isEnabled())
-			{
-				wb.sendKeys(value);
+	public static void sendKeys(WebElement wb, String value) {
+		if (wb.isDisplayed() && wb.isEnabled()) {
+			wb.sendKeys(value);
+		}
+	}
+
+	public static void click(WebElement wb) {
+		if (wb.isDisplayed() && wb.isEnabled()) {
+			wb.click();
+		}
+	}
+
+	public static String getUrl() {
+		return driver.getCurrentUrl();
+	}
+
+	public static String getTitle() {
+		return driver.getTitle();
+	}
+
+	public static void handleRadio(List<WebElement> wb, String value)
+
+	{
+		for (WebElement a : wb) {
+			if (a.getText().equals(value)) {
+				a.click();
+				break;
+
 			}
 		}
-	
-		public static void click(WebElement wb)
-		{
-			if(wb.isDisplayed()&&wb.isEnabled())
-			{
-				wb.click();
-			}
-		}
-		
-		public static String getUrl()
-		{
-			return driver.getCurrentUrl();
-		}
-		
-		public static String getTitle()
-		{
-			return driver.getTitle();
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
+
+	}
+
 }
